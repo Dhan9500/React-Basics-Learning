ad3cc14154731793c15d0ed98d727816b3ed82e8
Dhananjay Chauhan <dhananjay.chauhan9400@gmail.com>
2024-04-22 11:03:50 +0530

    prop drilling completed

--- src/Prop Drilling Exercise/Music.jsx
+++ src/Prop Drilling Exercise/Music.jsx
@@ -1,14 +1,15 @@
-export default function Music(){
+export default function Music({values,index,handleClick}){
+
     return(
         <div className="bg-zinc-500 w-56 h-28 flex justify-evenly mt-10 pt-2 pl- ml-4 pb-5 relative rounded-lg ">
         <div className="bg-white h-20 w-20 rounded-lg overflow-hidden">
-            <img className="h-full w-full  object-cover" src="https://media.istockphoto.com/id/1297349747/photo/hot-air-balloons-flying-over-the-botan-canyon-in-turkey.webp?b=1&s=170667a&w=0&k=20&c=1oQ2rt0FfJFhOcOgJ8hoaXA5gY4225BA4RdOP1RRBz4=" alt="" />
+            <img className="h-full w-full  object-cover" src={values.image} alt="" />
         </div>
         <div className="text-white">
-            <h1 className="font-semibold">Song Name</h1>
-            <h1 className="text-xs mt-2">Artist</h1>
+            <h1 className="font-semibold">{values.name}</h1>
+            <h1 className="text-xs mt-2">{values.artist}</h1>
         </div>
-        <button className="bg-red-500 absolute bottom-0 w-40 h-9 rounded-full whitespace-nowrap left-1/2 -translate-x-[50%] translate-y-[50%] text-white">Add to Favourites</button>
+        <button onClick={()=>handleClick(index)} className="bg-orange-500 absolute bottom-0 w-40 h-9 rounded-full whitespace-nowrap left-1/2 -translate-x-[50%] translate-y-[50%] text-white">{values.added?"Added":"Add to favourites"}</button>
         </div>
     )
 }
--- src/Prop Drilling Exercise/Navbar.jsx
+++ src/Prop Drilling Exercise/Navbar.jsx
@@ -1,10 +1,10 @@
-export default function Navbar(){
+export default function Navbar({fav}){
     return (
         <div className="px-5 py-10 font-bold text-xl w-full h-full flex justify-between items-center">
             <h1>Orange</h1>
-            <div className="bg-orange-500 h-11 w-32 rounded-lg flex justify-evenly items-center">
+            <div className="bg-orange-500 h-11 w-32 rounded-lg flex justify-evenly items-center text-white font-semibold">
             <h1>Favourites</h1>
-            <h1>2</h1>
+            <h1>{fav}</h1>
             </div>
         </div>
     )
